61ba81debf850a7f5a539f5b5e48c8d235d234de
Matthew Goggin <matthewgoggin@Matthews-MacBook-Pro.local>
2018-04-20 10:11:47 -0400

    Friend Finder

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friends = [
+	{
+		name:"Andy",
+		photo: "#",
+		scores: [1, 3, 1, 2, 3, 1, 1, 1, 1, 1]
+	},
+	{
+		name:"Rob",
+		photo: "#",
+		scores: [1, 3, 1, 2, 3, 1, 1, 4, 1, 1]
+	},
+	{
+		name:"Franklin",
+		photo: "#",
+		scores: [3, 3, 5, 2, 3, 1, 5, 1, 4, 1]
+	} 
+];
+
+module.exports = friends;
